82f57b16b12d7fa5f344ae5e7a43d0bf39c015e2
Pascal Mercat <pascal.mercat@gmail.com>
2018-06-27 22:34:33 +0200

    Modify to psycho version

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,21 @@
 function myFunction() {
     setTimeout(function(){ alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please"); }, 10000);
 }
+
+var psychButton = document.querySelector('button');
+
+function toggle() {
+    var el1 = document.getElementById("normal");
+    var el2 = document.getElementById("psych");
+    if (el2.disabled == "true") {
+        
+        
+    } else {
+        el1.disabled = "disabled";
+        el2.disabled = undefined;
+    }
+}
+
+psychButton.onclick = function() {
+    toggle();
+}
